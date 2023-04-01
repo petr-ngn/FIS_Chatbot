1a24b43497516dc9f1f035f9e4e12bb51a886948
petr-ngn <ngn.petr@gmail.com>
2023-04-01 19:38:22 +0200

    update of the init python script

--- src_PN/__init__.py
+++ src_PN/__init__.py
@@ -1,23 +1,27 @@
+import os
 import numpy as np
+import shutil
+import datetime
 import string
 import random 
 import nltk
-import numpy as num 
 import tensorflow as tf
 import keras
 from keras import backend as K
-import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras.regularizers import L2
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
 from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
 from keras_tuner.tuners import BayesianOptimization
-import os
 from bs4 import BeautifulSoup
 import requests
 import re
-import translators as ts
-import majka
+from majka import Majka
 import json
 import pickle
+from deep_translator import GoogleTranslator
+from keras import backend as K
+from nltk.corpus import stopwords
+from nltk import WordNetLemmatizer
